4261edb78d18ea65a2925c555b5abf341ccf156e
wonseok-han <oshan1112@gmail.com>
2024-10-21 12:13:32 +0900

    fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,21 @@
 import WebpackObfuscator from "webpack-obfuscator";
 
 const nextConfig = {
+  productionBrowserSourceMaps: false,
   webpack: (config, { isServer }) => {
     // 클라이언트 사이드 코드에만 난독화를 적용하도록 설정
     if (!isServer) {
+      //   config.optimization = {
+      //     splitChunks: {
+      //       cacheGroups: {
+      //         businessLogic: {
+      //           test: /[\\/]src[\\/]utils[\\/]/, // 비즈니스 로직을 포함하는 유틸리티 파일 등을 분리
+      //           name: "businessLogic",
+      //           chunks: "all",
+      //         },
+      //       },
+      //     },
+      //   };
       config.plugins.push(
         new WebpackObfuscator(
           {
@@ -13,7 +25,14 @@ const nextConfig = {
             stringArray: true,
             stringArrayThreshold: 0.75,
           },
-          ["static/**/*.js"] // 특정 파일들을 제외하거나 타겟으로 설정
+          [
+            // "static/**/*.js",
+            "**/node_modules/**", // 외부 라이브러리 제외
+            // "**/*.runtime.js", // runtime 관련 파일 제외
+            // "static/**/app/*.js",
+            // "static/**/app-pages-internals.js",
+            // "static/**/webpack.js*",
+          ] // 특정 파일들을 제외하거나 타겟으로 설정
         )
       );
     }
