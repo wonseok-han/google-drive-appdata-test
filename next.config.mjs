985d734adc0fb44681fb494bef1ee5b21c03ccd8
wonseok-han <oshan1112@gmail.com>
2024-10-21 11:16:17 +0900

    코드 난독화 테스트

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import WebpackObfuscator from "webpack-obfuscator";
+
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // 클라이언트 사이드 코드에만 난독화를 적용하도록 설정
+    if (!isServer) {
+      config.plugins.push(
+        new WebpackObfuscator(
+          {
+            rotateStringArray: true, // 난독화 옵션 설정
+            stringArray: true,
+            stringArrayThreshold: 0.75,
+          },
+          ["static/**/*.js"] // 특정 파일들을 제외하거나 타겟으로 설정
+        )
+      );
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
