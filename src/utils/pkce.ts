4601927f963936b297a2c77072dce7bc830ab3bf
wonseok-han <oshan1112@gmail.com>
2024-10-25 18:13:04 +0900

    fix pkce.ts

--- src/utils/pkce.ts
+++ src/utils/pkce.ts
@@ -1,18 +1,29 @@
-// utils/pkce.js
 export function generateCodeVerifier() {
   const randomBytes = new Uint8Array(32);
   window.crypto.getRandomValues(randomBytes);
-  return btoa(String.fromCharCode.apply(null, randomBytes))
+
+  // Uint8Array를 number 배열로 변환 후 처리
+  return btoa(
+    Array.from(randomBytes)
+      .map((byte) => String.fromCharCode(byte))
+      .join("")
+  )
     .replace(/\+/g, "-")
     .replace(/\//g, "_")
     .replace(/=+$/, "");
 }
 
-export async function generateCodeChallenge(codeVerifier) {
+export async function generateCodeChallenge(codeVerifier: string) {
   const encoder = new TextEncoder();
   const data = encoder.encode(codeVerifier);
   const digest = await window.crypto.subtle.digest("SHA-256", data);
-  return btoa(String.fromCharCode.apply(null, new Uint8Array(digest)))
+
+  // Uint8Array를 Array로 변환하여 처리
+  const base64String = Array.from(new Uint8Array(digest))
+    .map((byte) => String.fromCharCode(byte))
+    .join("");
+
+  return btoa(base64String)
     .replace(/\+/g, "-")
     .replace(/\//g, "_")
     .replace(/=+$/, "");
